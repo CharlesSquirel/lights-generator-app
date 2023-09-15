02dfcab923c85f861c84987128770fd3e726c7de
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-09-15 11:46:04 +0200

    Fix bugs after review

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,22 +3,26 @@ import IconMinus from "./icons/IconMinus";
 import style from "./Counter.module.css";
 import { useState } from "react";
 
-const Counter = ({ text}) => {
-    const [count, setCount] = useState(0)
-    const handleCountDecrease = () => {
-        setCount(count - 1)
+const Counter = ({ text }) => {
+  const [count, setCount] = useState(0);
+  const handleCountDecrease = () => {
+    if (count > 0) {
+      setCount(count - 1);
     }
-    const handleCountIncrease = () => {
-        setCount(count + 1)
+  };
+  const handleCountIncrease = () => {
+    if (count > 0) {
+      setCount(count + 1);
     }
+  };
   return (
     <div className={style.nav}>
       <p>
         {count} {text}
       </p>
       <div className={style.iconBox}>
-        <IconMinus onClick={handleCountDecrease}/>
-        <IconAdd onClick={handleCountIncrease}/>
+        <IconMinus onClick={handleCountDecrease} />
+        <IconAdd onClick={handleCountIncrease} />
       </div>
     </div>
   );
