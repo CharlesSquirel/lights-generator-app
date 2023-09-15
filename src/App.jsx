4b3fe5ab713ea5a92819dd30f901837fc0786c6e
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-09-15 11:40:19 +0200

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import Counter from "./components/Counter";
+import Text from "./components/Text"
 
 function App() {
-  return <div>Lights generator</div>;
+  return (
+    <div>
+      <nav className="nav">
+        <Text text="bulbs" count="0"/>
+        <Counter text="rows" />
+        <Counter text="columns" />
+      </nav>
+    </div>
+  );
 }
 
 export default App;
